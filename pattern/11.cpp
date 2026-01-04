fa4c6128b312e01d67064da425ae523f15b6bb5e
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2026-01-04 01:27:29 +0000

    second

--- pattern/11.cpp
+++ pattern/11.cpp
@@ -1,12 +1,9 @@
-
 /*
 
-1 2 3 4 
-  5 6 7 
-    8 9 
-      10 
-
-
+1 
+2 3 
+4 5 6 
+7 8 9 10 
 
 */
 
@@ -21,11 +18,8 @@ int main(){
     int count=1;
 
     for(int i=0;i<n;i++){
-        for(int j=0;j<i;j++){
-        cout<<"  ";
-        }
 
-        for(int k=0;k<n-i;k++){
+        for(int k=0;k<=i;k++){
         cout<<count++<<" ";
         }
 
